b8e95a7317caf4f12a8e2da9935b308902bcd5c4
Marwane Bazi <mbazi@iesebre.com>
2023-02-07 19:50:33 +0100

    Actualitzo

--- UF2/ONG/src/main/java/ONG.java
+++ UF2/ONG/src/main/java/ONG.java
@@ -1,4 +1,6 @@
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -12,15 +14,17 @@ public class ONG {
     public static Scanner input = new Scanner(System.in);
     public static int usuari, KgDemanats;
 
+    public static String text;
+
 
     public static void main(String[] args) {
         java.util.Date fecha = new Date();
-        System.out.println (fecha);
+        System.out.println(fecha);
         menu();
 
     }
 
-    public static void menu(){
+    public static void menu() {
         System.out.println("SELECCIONA UNA OPCIÓ:");
         System.out.println("1. Donar d'alta Associació");
         System.out.println("2. Aliments");
@@ -30,7 +34,7 @@ public class ONG {
 
         usuari = input.nextInt();
 
-        while (usuari < 0 || usuari > 4){
+        while (usuari < 0 || usuari > 4) {
             System.out.println("Tria una opció vàlida");
             usuari = input.nextInt();
         }
@@ -62,32 +66,101 @@ public class ONG {
     }
 
 
-    public static void menuAssociacio(){
+    public static void menuAssociacio() {
+        int n = distancia.length;
+        int m = associacio.length;
+
         System.out.println("DONAR D'ALTA ASSOCIACIÓ");
-        System.out.println("Nom");
-        System.out.println("telefon");
-        System.out.println("distancia");
         System.out.println("0. Enrere");
+
+        System.out.println("Introdueix el nom:");
+        text = input.skip("\n").nextLine();
+        if (text.equals("0")) {
+            menu();
+        } else {
+            associacio = Arrays.copyOf(associacio, m + 1);
+            associacio[m] = m + ". " + text;
+            System.out.println("Introdueix el telèfon");
+            text = input.nextLine();
+
+            System.out.println("Introdueix la distancia");
+            usuari = input.nextInt();
+            distancia = Arrays.copyOf(distancia, n + 1);
+            distancia[n] = usuari;
+
+            System.out.println(Arrays.toString(associacio));
+
+            menu();
+        }
+
     }
 
-    public static void aliments(){
+    public static void aliments() {
+        int numSeleccionat, numAliments, cond;
+        String[] aliments = new String[10];
+
         System.out.println("ALIMENTS");
-        System.out.println("1. Donar d'alta");
-        System.out.println("2. Mostrar aliments");
-        System.out.println("3. Modificar nom");
-        System.out.println("4. Eliminar");
-        System.out.println("0. Enrere");
-    }
 
-    public static void enviaments(){
+        System.out.println("Vols seguir o tornar atras? (0 per tornar atras i qualsevol altre NÚMERO per a seguir)");
+        cond = input.nextInt();
+        if (cond == 0) {
+            menu();
+        } else {
+            System.out.println("Quants aliments vols introduïr?");
+            numAliments = input.nextInt();
+            for (int j = 0; j < numAliments + 1; j++) {
+                aliments[j] = input.nextLine();
+            }
+            System.out.println("ALIMENTS");
+            System.out.println(Arrays.toString(aliments));
+
+            //Per a modificar
+            System.out.println("Vols modificar algún aliment? (1 per a modificar i qualsevol altre NÚMERO per seguir)");
+            cond = input.nextInt();
+            if (cond == 1) {
+                System.out.println("Quin aliment vols modificar?");
+                System.out.println(Arrays.toString(aliments));
+                numSeleccionat = input.nextInt();
+
+                System.out.println("Has seleccionat " + aliments[numSeleccionat] + " , introdueix el nou nom");
+
+                aliments[numSeleccionat] = input.skip("\n").nextLine();
+                System.out.println("Ha quedat així:");
+                System.out.println(Arrays.toString(aliments));
+
+
+            }
+            System.out.println("Vols borrar algún aliment? (1 per a borrar i qualsevol altre NÚMERO per seguir)");
+            cond = input.nextInt();
+            if (cond == 1) {
+                String condBorrat;
+
+                System.out.println("Introdueix un número per dir quin aliment vols BORRAR, hi ha les següents opcions:,");
+                System.out.println(Arrays.toString(aliments));
+                numSeleccionat = input.nextInt();
+
+                //aixó és per a esborrar l'aliment que hem seleccionat si escrivim esborrar
+
+                System.out.println("Has seleccionat " + aliments[numSeleccionat]);
+                System.out.println("Escriu 'BORRAR' per a borrar l'aliment, per a parar posa qualsevol altra cosa o INTRO.");
+                condBorrat = input.skip("[\r\n]*").nextLine();
+                //Condició de borrat
+                if (condBorrat.equals("BORRAR") || condBorrat.equals("Borrar") || condBorrat.equals("borrar")) {
+                    aliments[numSeleccionat] = null;
+                }
+                System.out.println("Ha quedat així:");
+                System.out.println(Arrays.toString(aliments));
+
+            }
+            System.out.println();
+            menu();
+        }
+    }
+    public static void enviaments() {
         int costEnviament;
 
         System.out.println("ENVIAMENTS");
         System.out.println("0. Enrere");
-        System.out.println("seleccionar aliments");
-        System.out.println("suma de kilos");
-        System.out.println("calcular");
-        System.out.println("data enviament");
 
         // Demanem a l'usuari que seleccioni l'associacio on s'enviarà i guardem la distància.
         System.out.println("Selecciona la associacio:" + Arrays.toString(associacio));
@@ -100,7 +173,7 @@ public class ONG {
         usuari = input.nextInt();
 
         // Tornar enrere
-        if (usuari == 0){
+        if (usuari == 0) {
             menu();
         }
 
@@ -125,23 +198,24 @@ public class ONG {
                     } else {
                         System.out.println("El cost de l'enviament és: " + costEnviament * 0.22 + " €");
                     }
-                    System.out.println("Mostrar data");
+
+                    String Data = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
+                    System.out.println("La teva comanda arribarà " + Data);
 
                 } else System.out.println("Demanes més KG del que hi ha en stock");
 
             } else System.out.println("No hi ha stock");
         }
+        menu();
 
     }
 
-    public static void donacions(){
+    public static void donacions() {
         System.out.println("FER DONACIONS");
         System.out.println("1. Seleccionar aliment");
         System.out.println("0. Enrere");
 
         usuari = input.nextInt();
-
-
 
         do {
             if (usuari == 0) {
@@ -151,12 +225,12 @@ public class ONG {
                 System.out.println(Arrays.toString(aliments));
                 usuari = input.nextInt();
 
-                if (usuari == 0){
+                if (usuari == 0) {
                     donacions();
                 } else {
                     System.out.println("Quants KG vols donar");
                     KgDemanats = input.nextInt();
-                    stock[usuari -1] = (stock[usuari - 1] + KgDemanats);
+                    stock[usuari - 1] = (stock[usuari - 1] + KgDemanats);
                     System.out.println("Has seleccionat " + aliments[usuari]);
                     System.out.println("Hi ha un total de " + stock[usuari - 1] + " KG en stock");
                     break;
@@ -168,8 +242,6 @@ public class ONG {
             usuari = input.nextInt();
 
         } while (true);
-
-
 
         menu();
     }
